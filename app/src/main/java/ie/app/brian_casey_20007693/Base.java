466ccb0aa040832bc74e813183ad74ace54fdce4
kcboy303 <Casey@Brians-MBP.lan>
2017-02-26 16:43:58 +0000

    Fist Commit
    List app. Fist Activity almost complete

---
+++ app/src/main/java/ie/app/brian_casey_20007693/Base.java
@@ -0,0 +1,28 @@
+package ie.app.brian_casey_20007693;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import java.util.HashSet;
+import java.util.Set;
+import android.app.AlertDialog;
+import android.widget.EditText;
+import android.content.DialogInterface;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.app.Activity;
+
+public class Base {
+}
